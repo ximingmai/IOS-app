8466217708c0bbca6b2d0fe4aad7db9430b569b4
Staryyan <yzx9610@outlook.com>
2016-06-27 15:45:14 +0800

    just for test

--- codes/Cilly/ViewController.swift
+++ codes/Cilly/ViewController.swift
@@ -9,9 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+  var name : String = "test"
   override func viewDidLoad() {
     super.viewDidLoad()
+    print(name)
     // Do any additional setup after loading the view, typically from a nib.
   }
 
